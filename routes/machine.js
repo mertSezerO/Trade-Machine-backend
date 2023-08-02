f414068a8c7ffe59daf9beb86d1e76f86532a749
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-08-02 09:44:43 +0300

    Unnecessary endpoints deleted.

--- routes/machine.js
+++ routes/machine.js
@@ -1,12 +1,10 @@
-const express = require('express')
-const Router = express.Router()
+const express = require("express");
+const Router = express.Router();
 
-const machineController = require('../controllers/machine')
+const machineController = require("../controllers/machine");
 
-Router.get('/teams', machineController.getTeams)
+Router.get("/teams", machineController.getTeams);
 
-Router.get('/teams/:teamId', machineController.getTeamPlayers)
+Router.get("/teams/:teamId", machineController.getTeamPlayers);
 
-Router.get('/players/:playerId', machineController.getPlayer)
-
-module.exports = Router
+module.exports = Router;
